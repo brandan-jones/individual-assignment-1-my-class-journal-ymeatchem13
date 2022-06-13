72e0a4c1363d3f0b410aedc702978ffd7cdb42bc
ymeatchem13 <meatchemyoshua@yahoo.com>
2022-06-13 17:48:12 -0400

    Created missing methods & added missing dependencies

--- src/main/java/edu/uc/cech/soit/myclassjournal/MyClassJournalController.java
+++ src/main/java/edu/uc/cech/soit/myclassjournal/MyClassJournalController.java
@@ -1,10 +1,12 @@
 package edu.uc.cech.soit.myclassjournal;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class MyClassJournalController {
     /**
-     * Handle the / endpoint
+     * Handle the root (/) endpoint and return a start page
      * @return
      */
     @RequestMapping("/")
---
+++ src/main/java/edu/uc/cech/soit/myclassjournal/dto/JournalEntry.java
@@ -0,0 +1,19 @@
+package edu.uc.cech.soit.myclassjournal.dto;
+
+import lombok.Data;
+
+public @Data class JournalEntry {
+    public void setNotes(String notes) {
+    }
+
+    public String getNotes() {
+        return "";
+    }
+
+    public void setDate(String date) {
+    }
+
+    public String getDate() {
+        return "";
+    }
+}
